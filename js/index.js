7946fd6a8b8a5b1b5e061010ccd74b7ba27af66a
Dekel Livyani <DekelLiv0@gmail.com>
2021-06-06 18:53:08 +0300

    finish day 3

--- js/index.js
+++ js/index.js
@@ -3,3 +3,20 @@
 function OpenMenu() {
     document.body.classList.toggle('menu-open');
 }
+
+function openReadMore() {
+    document.body.classList.add('more-open');
+    var elModal = document.querySelector('.modal-read-more');
+    elModal.style.display = "inline-block";
+}
+
+function closeModal() {
+    document.body.classList.remove('more-open');
+    var elModal = document.querySelector('.modal-read-more');
+    elModal.style.display = "none";
+    document.querySelector('#email-text').value = '';
+}
+
+function sendToEmail(ev) {
+    ev.stopPropagation();
+}
