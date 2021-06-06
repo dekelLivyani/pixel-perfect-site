7d3a139cc29791be935a4ba2c8156d4548244ed7
Dekel Livyani <DekelLiv0@gmail.com>
2021-06-06 16:51:43 +0300

    finish menu

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 'use strict'
 
-function overMoreRead(elBtnMoreRead) {
-    elBtnMoreRead.innerText += '\xa0\xa0 ➡';
-}
-
-function overOutMoreRead(elBtnMoreRead) {
-    var lenght = elBtnMoreRead.innerText.length;
-    elBtnMoreRead.innerText = elBtnMoreRead.innerText.substring(0, lenght - 4);
+function OpenMenu() {
+    document.body.classList.toggle('menu-open');
 }
